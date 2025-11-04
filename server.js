0bdecb18b71a6db88df71f2172c8773a606efdde
Adriel Colombo de Sousa <adrielcolombodesousa@gmail.com>
2025-11-04 11:22:44 -0300

    Alteração para adicionar rota 2 e rota 3

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ app.get('/',(req,res) => {
     res.send('Olá mundo')
 });
 
+//rota 2
+app.get('/sobre',(req,res) => {
+    res.send('Esta é a rota sobre o sistema')
+});
+
+
+//rota 3
+app.get('/usuarios',(req,res) => {
+    res.json([
+    {id: 1, nome: 'Daniel'},
+    {id: 2, nome: 'Carolina'}    
+]);
+});
+
 //inicia o servidor
 app.listen(3000, () => {
     console.log('Servidor rodado em http://localhost:3000')
-})
+});
